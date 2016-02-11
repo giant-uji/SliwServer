c43d427e45e914cb8dbde79f89494c4f9eea0e28
Adrian Puertas Cabedo <al259348@uji.es>
2016-02-11 09:23:01 +0100

    UserService obtener usuario vinculado a un dispositivo concreto

--- src/main/java/es/uji/al259348/sliwserver/services/UserService.java
+++ src/main/java/es/uji/al259348/sliwserver/services/UserService.java
@@ -1,5 +1,8 @@
 package es.uji.al259348.sliwserver.services;
 
+import es.uji.al259348.sliwserver.model.User;
+
 public interface UserService {
 
+    User getUserLinkedTo(String deviceId);
 }
--- src/main/java/es/uji/al259348/sliwserver/services/UserServiceImpl.java
+++ src/main/java/es/uji/al259348/sliwserver/services/UserServiceImpl.java
@@ -1,10 +1,25 @@
 package es.uji.al259348.sliwserver.services;
 
+import es.uji.al259348.sliwserver.model.Device;
+import es.uji.al259348.sliwserver.model.User;
+import es.uji.al259348.sliwserver.repositories.DeviceRepository;
+import es.uji.al259348.sliwserver.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserRepository userRepository;
 
+    @Autowired
+    DeviceRepository deviceRepository;
+
+    @Override
+    public User getUserLinkedTo(String deviceId) {
+        Device device = deviceRepository.findOne(deviceId);
+        return (device != null) ? device.getUser() : null;
+    }
 
 }
