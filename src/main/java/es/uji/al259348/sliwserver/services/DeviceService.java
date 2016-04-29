c882343ea1b9c4a60b04a0ead49bd735592010e8
Adrian Puertas Cabedo <al259348@uji.es>
2016-04-29 13:44:10 +0200

    DeviceService

---
+++ src/main/java/es/uji/al259348/sliwserver/services/DeviceService.java
@@ -0,0 +1,9 @@
+package es.uji.al259348.sliwserver.services;
+
+import es.uji.al259348.sliwserver.model.Device;
+
+public interface DeviceService {
+
+    Device save(Device device);
+
+}
---
+++ src/main/java/es/uji/al259348/sliwserver/services/DeviceServiceImpl.java
@@ -0,0 +1,19 @@
+package es.uji.al259348.sliwserver.services;
+
+import es.uji.al259348.sliwserver.model.Device;
+import es.uji.al259348.sliwserver.repositories.DeviceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeviceServiceImpl implements DeviceService {
+
+    @Autowired
+    DeviceRepository deviceRepository;
+
+    @Override
+    public Device save(Device device) {
+        return deviceRepository.save(device);
+    }
+
+}
